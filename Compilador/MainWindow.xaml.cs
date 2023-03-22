acfb8337909406d755e599ac0bb6d7ec3cf645e2
Julio Cesar Castro <julioCC@estudiantec.cr>
2023-03-22 11:39:22 -0600

    Cambios a la interfaz
    Se agrego el boton de Upload file, para cargar el archivo data.txt al textBox que tiene por nombre TxtBox, falta la logica

--- Compilador/MainWindow.xaml.cs
+++ Compilador/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Controls;
 using Antlr4.Runtime;
 
 
@@ -14,42 +13,41 @@ namespace Compilador
             InitializeComponent();
         }
 
-        private void Run_Button_Click(object sender, RoutedEventArgs e) {
+        private void Run_Button_Click(object? sender, RoutedEventArgs e) {
             Console.WriteLine("Success!");
             // Get the text from the TxtBox
-            //TextBox TxtBox = this.FindControl<TextBox>("TxtBox");
-            var text = TxtBox.Text;
-            
+            // object TxtBox = this.FindName("TxtBox");
+            // var text = TxtBox.ToString();
             try
             {
-                ICharStream input = CharStreams.fromString(text);
+                ICharStream input =
+                    CharStreams.fromPath(
+                        "C:\\Users\\jjuli\\RiderProjects\\MiniCSharp\\MiniCSharp\\Components\\data.txt");
                 AlphaScanner lexer = new AlphaScanner(input);
                 CommonTokenStream tokens = new CommonTokenStream(lexer);
                 AlphaParser parser = new AlphaParser(tokens);
                 parser.program();
-                var tree = parser.program();
+                AlphaParser.ProgramContext tree = parser.program();
                 // Print the result
-                Console.WriteLine("Success!");
-                
-                //Print the tree
-                Console.WriteLine(tree.ToStringTree(parser));
+                Console.Write("Success!");
 
+                //Print the tree
+                Console.Write(tree.ToStringTree(parser));
             }
             catch (Exception exception)
             {
-                
-                Console.WriteLine(exception);
+                Console.Write("Error!");
+                Console.Write(exception);
                 throw;
             }
-        
-
-        }
-        private void Build_Button_Click(object sender, RoutedEventArgs e) {
-
-        }
-        private void Exit_Button_Click(object sender, RoutedEventArgs e)
-        {
             
+        }
+        private void Build_Button_Click(object? sender, RoutedEventArgs e) {
+                 //aqui va el codigo para cargar el data.txt en el txtbox
+                   
+        }
+        private void Exit_Button_Click(object? sender, RoutedEventArgs e)
+        {
             Application.Current.Shutdown();
         }
     }
